d6a87b897187536d894a3ca190ec664d185529cd
grewalr <ricky@mcgrewal.com>
2016-08-10 18:09:31 +0100

    First attempt implementation

--- wk04-calculator/src/main/scala/calculator/Polynomial.scala
+++ wk04-calculator/src/main/scala/calculator/Polynomial.scala
@@ -1,13 +1,40 @@
 package calculator
 
-object Polynomial {
+object Polynomial
+{
   def computeDelta(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double]): Signal[Double] = {
-    ???
+                   c: Signal[Double]): Signal[Double] =
+  {
+    // ax² + bx + c
+    // b² - 4ac
+    Signal
+    {
+      val x = a()
+      val y = b()
+      val z = c()
+      (y * y) - (4 * x * z)
+    }
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+                       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] =
+  {
+    // (-b ± √Δ) / 2a
+    Signal
+    {
+      val x = a()
+      val y = b()
+      val z = c()
+      val d = delta()
+
+      if(d > 0)
+      {
+        Set(
+          BigDecimal(((-y) + Math.sqrt(d)) / (2 * x)).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble,
+          BigDecimal(((-y) - Math.sqrt(d)) / (2 * x)).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+        )
+      }
+      else Set(0.0, 0.0)
+    }
   }
 }
